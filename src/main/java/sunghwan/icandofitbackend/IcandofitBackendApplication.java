a4b2e2b0ef835c4c98b3b6f1d4b03bedfc974384
sunghwan-ds <jeonsung02@gmail.com>
2023-05-13 17:56:10 +0900

    Create icandofit-backend Project

---
+++ src/main/java/sunghwan/icandofitbackend/IcandofitBackendApplication.java
@@ -0,0 +1,13 @@
+package sunghwan.icandofitbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcandofitBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IcandofitBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sunghwan/icandofitbackend/IcandofitBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package sunghwan.icandofitbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IcandofitBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
